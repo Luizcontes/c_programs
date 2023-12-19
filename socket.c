f9b4d92dd4a6fadc88f81657677e9352c6aa46cf
Luiz <luiz.contes@hotmail.com>
2023-12-19 18:55:15 +0000

    new commit

---
+++ socket.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+
+    int keepConnection = 1;
+    while (keepConnection) {
+        int serverSocket, clientSocket;
+        struct sockaddr_in serverAddr, clientAddr;
+        socklen_t clientAddrLen = sizeof(clientAddr);
+        char buffer[BUFFER_SIZE];
+
+        // Create socket
+        if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+            perror("Socket creation failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Set up server address structure
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_addr.s_addr = INADDR_ANY;
+        serverAddr.sin_port = htons(PORT);
+
+        // Bind socket to address and port
+        if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+            perror("Bind failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Listen for incoming connections
+        if (listen(serverSocket, 5) == -1) {
+            perror("Listen failed");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Server listening on port %d...\n", PORT);
+
+        // Accept incoming connection
+        if ((clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &clientAddrLen)) == -1) {
+            perror("Accept failed");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Connection accepted from %s:%d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+
+        // Receive data from client
+        ssize_t bytesRead;
+        while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
+            buffer[bytesRead] = '\0';  // Null-terminate the received data
+            printf("Received data: %s", buffer);
+        }
+
+        if (bytesRead == -1) {
+            perror("Receive failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Close the sockets
+        close(clientSocket);
+        close(serverSocket);
+
+
+    }
+
+    return 0;
+}
---
+++ write.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+    // File descriptor for standard output (stdout)
+    int fileDescriptor = 1;
+
+    // String to be written
+    const char *message = "Hello, write() function!\n";
+
+    int size_t = 0;
+    while(message[size_t] != '\0') {
+        size_t++;
+    }
+
+    // Using the write() function to write to stdout
+    ssize_t bytesWritten = write(fileDescriptor, message, size_t);
+
+    // printf("write call returned number: %zd", bytesWritten);
+
+    // Checking for errors
+    // if (bytesWritten == -1) {
+    //     // Handle error if write() fails
+    //     return 1;
+    // }
+
+    return 0;
+}
